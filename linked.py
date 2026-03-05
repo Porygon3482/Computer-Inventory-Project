c231ed1b006550c6bd03ca957f16f6c6b013012e
MoonyCheese <kneeljhala@gmail.com>
2026-03-04 22:48:00 -0800

    Add files via upload

--- linked.py
+++ linked.py
@@ -1,12 +1,63 @@
+"""
+Name: Neel Jhala
+Assignment: Project 2, linked.py
+Date: 3/4/2026
+Summary:
+This file creates the node and linked computer classes used to establish the linked structure computer database in 
+the main code
+"""
+from Computer import ComputerSystem
 class Node():
+    """
+    Singular node in a linked structure
+    """
     def __init__(self, data, next=None):
+        """
+        :param data: The data contained by the node
+        :param next: The next node in the linked structure
+        """
         self.data = data
         self.next = next
 
 class linkedComputer():
+    """
+    Bag implementation of a linked structure that contains Computer classes as data
+    """
+
     def __init__(self):
         self._items = None
         self._size=0
     
-    def add(computer):
-        
+    def __iter__(self):
+        """
+        Sets up iteration through the linked structure using a cursor that moves through each node and returns its
+        data
+        """
+        cursor = self._items
+        while not cursor is None:
+            yield cursor.data
+            cursor = cursor.next
+    
+    def add(self, computerData):
+        """
+        Adds a computer to the linked structure, inserting by year purchased
+        :param computerData: the computer being added(must be of the computer class)
+        """
+        for computer in self:
+            if computer.data.yearPurchased<=computerData.yearPurchased:
+                computerIndex = computer
+                break
+        computerNode = Node(computerData, computerIndex.next)
+        computerIndex.next = computerNode
+        self._size+=1
+    
+    def remove(self):
+        """
+        Removes and returns data of the first node in the linked structure(the oldest computer)
+        """
+        for computer in self:
+            if computer==0:
+                data = computer
+                break
+        data.next = None
+        return data
