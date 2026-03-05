a6aadbf83d746b271298c43eb8ab490f54b7ee5e
MoonyCheese <kneeljhala@gmail.com>
2026-03-04 22:44:35 -0800

    Add files via upload

---
+++ linked.py
@@ -0,0 +1,12 @@
+class Node():
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+class linkedComputer():
+    def __init__(self):
+        self._items = None
+        self._size=0
+    
+    def add(computer):
+        
